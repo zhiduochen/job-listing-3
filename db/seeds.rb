85848c82ad8ceb338f6d1ef5e4d3701be84e67ac
chenzhiduo <zhiduochen@gmail.com>
2017-02-02 20:29:48 +0800

    step 11

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
 
 puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 public jobs, 以及10個hidden jobs"
 
-create_account = User.create([email: 'dor211@163.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
+create_account = User.create([email: 'dor211@163.com', password: '12345678', password_confirmation: '12345678', is_admin=true)
 puts "Admin account created."
 
 create_jos = for i in 1..10 do
